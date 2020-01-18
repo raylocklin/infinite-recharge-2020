cc8a6d2521105af9842ab3f6c03956c62c415100
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-18 18:32:44 -0500

    Update and add motorDefs

---
+++ InfiniteChargez/src/main/cpp/hardware/Motors.cpp
@@ -0,0 +1,5 @@
+#include "MotorDef.h"
+#include "frc/PWMVictorSPX.h"
+
+frc::PWMVictorSPX driveMotorLeft{0};
+frc::PWMVictorSPX driveMotorRight{1};
---
+++ InfiniteChargez/src/main/include/MotorDef.h
@@ -0,0 +1,10 @@
+#ifndef YEET_CONTROLLER_DEFINITION_H
+#define YEET_CONTROLLER_DEFINITION_H
+#include <frc/PWMVictorSPX.h>
+
+extern frc::PWMVictorSPX driveMotorLeft;
+extern frc::PWMVictorSPX driveMotorRight;
+
+
+
+#endif

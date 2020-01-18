adcd60e9a49af2d7ed865ed7a85de758ea2a7f1b
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-18 18:43:20 -0500

    Add controller definitions and implementations

---
+++ InfiniteChargez/src/main/cpp/control/Controllers.cpp
@@ -0,0 +1,4 @@
+#include "ControllerDef.h"
+#include <frc/XboxController.h>
+const int controllerPort{0};
+frc::XboxController leController{controllerPort}; //Make this configurable outside
---
+++ InfiniteChargez/src/main/include/ControllerDef.h
@@ -0,0 +1,8 @@
+#ifndef YEET_CONTROLLER_DEFINITIONS_H
+#define YEET_CONTROLLER_DEFINITIONS_H
+#include <frc/XboxController.h>
+namespace control
+{
+extern frc::XboxController leController; //Of epic dankness
+}
+#endif

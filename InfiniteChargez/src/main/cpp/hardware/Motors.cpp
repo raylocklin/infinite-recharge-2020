cbc4c7e967cfc9a2cdf961d5e6ea3f00514d766f
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-19 21:24:48 -0500

    Motor Definitions, Implementations, Port Update
    More motors have been added in anticipation for the
    "Everybot" design. The Magic numbers at the ports have
    been removed and replaced with constant expressions.

--- InfiniteChargez/src/main/cpp/hardware/Motors.cpp
+++ InfiniteChargez/src/main/cpp/hardware/Motors.cpp
@@ -2,6 +2,24 @@
 #include "frc/PWMVictorSPX.h"
 namespace hardware
 {
-    frc::PWMVictorSPX driveMotorLeft{0};
-    frc::PWMVictorSPX driveMotorRight{1};
+    
+    /*The integer passed into the motors' constructor
+    corresponds to the port to which they were plugged into
+    the RoboRio*/
+    constexpr int portDriveLeft{0};
+    constexpr int portDriveRight{1};
+    constexpr int portIntakeLeft{2};
+    constexpr int portIntakeRight{3};
+    constexpr int portStorage{4};
+    constexpr int portHook{5};
+
+    driveMotor_t driveMotorLeft{portDriveLeft};
+    driveMotor_t driveMotorRight{portDriveRight};
+
+    intakeMotor_t intakeMotorLeft{portIntakeLeft};
+    intakeMotor_t intakeMotorRight{portIntakeRight};
+
+    storageMotor_t storageMotor{portStorage};
+
+    hookMotor_t hookMotor{portHook};
 }
--- InfiniteChargez/src/main/include/MotorDef.h
+++ InfiniteChargez/src/main/include/MotorDef.h
@@ -3,8 +3,21 @@
 #include <frc/PWMVictorSPX.h>
 
 namespace hardware
-{
-    extern frc::PWMVictorSPX driveMotorLeft;
-    extern frc::PWMVictorSPX driveMotorRight;
+{   
+    using driveMotor_t = frc::PWMVictorSPX;
+    using intakeMotor_t = frc::PWMVictorSPX;
+    using storageMotor_t = frc::PWMVictorSPX;
+    using hookMotor_t = frc::PWMVictorSPX;
+
+    extern driveMotor_t driveMotorLeft;
+    extern driveMotor_t driveMotorRight;
+
+    extern intakeMotor_t intakeMotorLeft;
+    extern intakeMotor_t intakeMotorRight;
+
+    extern storageMotor_t storageMotor;
+
+    extern hookMotor_t hookMotor;
+    
 }
 #endif

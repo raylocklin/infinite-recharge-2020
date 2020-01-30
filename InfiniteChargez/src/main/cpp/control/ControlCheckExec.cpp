eacd763edddd2cc92c0eeaba46712edb27cd89a3
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-30 15:10:46 -0600

    Integrate Squarification into the controller workflow

--- InfiniteChargez/src/main/cpp/control/ControlCheckExec.cpp
+++ InfiniteChargez/src/main/cpp/control/ControlCheckExec.cpp
@@ -1,4 +1,6 @@
 #include "Robot.h"
+#include "Utilities.h"
+#include "Pair2D.h"
 #include <algorithm>
 #include <iostream>
 #include <math.h>
@@ -15,11 +17,16 @@
     
     void Robot::joystickPosition()
     {
+        //Compilar arguments and preprocessor macros can be passed to remove unused
         const double JoystickLeftX{leController.GetX(frc::GenericHID::JoystickHand::kLeftHand)};
         const double JoystickLeftY{leController.GetY(frc::GenericHID::JoystickHand::kLeftHand)};
 
         const double JoystickRightX{leController.GetX(frc::GenericHID::JoystickHand::kRightHand)};
         const double JoystickRightY{leController.GetY(frc::GenericHID::JoystickHand::kRightHand)};
+        
+        //Squarified Values
+        const Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
+        const Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
 
 
         if(tankMode)
@@ -29,8 +36,8 @@
         }
         else 
         {
-            driveMotorsRight.Set(-std::clamp(JoystickLeftY - JoystickLeftX, -1.0, 1.0));
-            driveMotorsLeft.Set(std::clamp(JoystickLeftY + JoystickLeftX, -1.0, 1.0));//FIGURE OUT WHY A NEGATIVE IS NEEDED!!!!
+            driveMotorsRight.Set(-std::clamp(SquareJoystickLeft.y - SquareJoystickLeft.x, -1.0, 1.0));
+            driveMotorsLeft.Set(std::clamp(SquareJoystickLeft.y + SquareJoystickLet.x, -1.0, 1.0));//FIGURE OUT WHY A NEGATIVE IS NEEDED!!!!
         }
             /*driverMain.ArcadeDrive(
             leController.GetY(frc::GenericHID::JoystickHand::kLeftHand),

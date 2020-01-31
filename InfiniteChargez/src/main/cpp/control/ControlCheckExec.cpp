83ff10a98a82ad991c892e6b0834aeceadf8cf98
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-30 18:32:33 -0600

    Invert X axis for Xbox controller since Up is negative in it's api. THANK YOU PROGRAMMERS

--- InfiniteChargez/src/main/cpp/control/ControlCheckExec.cpp
+++ InfiniteChargez/src/main/cpp/control/ControlCheckExec.cpp
@@ -19,16 +19,18 @@
     {
         //Compilar arguments and preprocessor macros can be passed to remove unused
         const double JoystickLeftX{leController.GetX(frc::GenericHID::JoystickHand::kLeftHand)};
-        const double JoystickLeftY{leController.GetY(frc::GenericHID::JoystickHand::kLeftHand)};
+        //Up is negative in Xbox controllers
+        const double JoystickLeftY{-leController.GetY(frc::GenericHID::JoystickHand::kLeftHand)};
 
         const double JoystickRightX{leController.GetX(frc::GenericHID::JoystickHand::kRightHand)};
-        const double JoystickRightY{leController.GetY(frc::GenericHID::JoystickHand::kRightHand)};
+        //Up isnegative in Xbox controllers
+        const double JoystickRightY{-leController.GetY(frc::GenericHID::JoystickHand::kRightHand)};
         
         //Squarified Values
         const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
         const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
 
-        std::cout << "X: " << SquareJoystickLeft.x << " Y: " << SquareJoystickLeft.y;
+        //std::cout << "X: " << SquareJoystickLeft.x << " Y: " << SquareJoystickLeft.y << '\n';
 
 
         if(tankMode)
@@ -38,8 +40,8 @@
         }
         else 
         {
-            driveMotorsRight.Set(-std::clamp(SquareJoystickLeft.y - SquareJoystickLeft.x, -1.0, 1.0));
-            driveMotorsLeft.Set(std::clamp(SquareJoystickLeft.y + SquareJoystickLeft.x, -1.0, 1.0));//FIGURE OUT WHY A NEGATIVE IS NEEDED!!!!
+            //driveMotorsRight.Set(std::clamp(SquareJoystickLeft.y - SquareJoystickLeft.x, -1.0, 1.0));
+            //driveMotorsLeft.Set(std::clamp(SquareJoystickLeft.y + SquareJoystickLeft.x, -1.0, 1.0));//FIGURE OUT WHY A NEGATIVE IS NEEDED!!!!
         }
             /*driverMain.ArcadeDrive(
             leController.GetY(frc::GenericHID::JoystickHand::kLeftHand),

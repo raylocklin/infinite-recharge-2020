2f9641389949cd2c6c6ad6da6971d700026f4ff3
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-30 20:32:18 -0600

    Clean up-ish Squareification code and const optimization
    Make all variables that are not modified const
    in order to optimize preformance.
    
    Debug code is commented out but will be kept
    until the code is actually tested on the robot.

--- InfiniteChargez/src/main/cpp/control/Utilities.cpp
+++ InfiniteChargez/src/main/cpp/control/Utilities.cpp
@@ -12,10 +12,10 @@ namespace utilities
     Pair2D<double> squarify(double x, double y)
     {
         //std::cout << "X: " << x << "Y: " << y << '\n';
-        double angle{std::atan2(y, x)}; //Why doesthe angle keep returning intermittently to 2.75099
+        const double angle{std::atan2(y, x)}; //Why doesthe angle keep returning intermittently to 2.75099
         //std::cout << "Angle: " << angle << '\n';
         //Take the percentage from the origin to the edge of the circle
-        double circleRadiusPercent{std::clamp(std::sqrt(x * x + y * y), 0.0, 1.0)}; //Ensure that percent is maxmimum of 1 due to joystick imperfections
+        const double circleRadiusPercent{std::clamp(std::sqrt(x * x + y * y), 0.0, 1.0)}; //Ensure that percent is maxmimum of 1 due to joystick imperfections
         //std::cout << "Percent: " << circleRadiusPercent << '\n';
         /*Find the maximum distance to a square whose sides are the diameter of the circle from the angle
         *The formula used is the radial equation for a circle by Raven Jyro Felix which uses modulo */
@@ -24,14 +24,14 @@ namespace utilities
         *Since the formula offsets the angle by pi/2, it must be added to make
         *The circle upright
         */
-        double processedAngleForSquare{(std::abs(std::fmod((angle + M_PI_4), M_PI_2))) - M_PI_4};
+        const double processedAngleForSquare{(std::abs(std::fmod((angle + M_PI_4), M_PI_2))) - M_PI_4};
         //Since fmod returns negative if the input is negative, it must be made positive in order to align with mathematical modulo
         //std::cout <<  processedAngleForSquare;
         double squareRadiusMax{std::sqrt(std::pow(std::tan(processedAngleForSquare), 2.0) + 1)};
-        std::cout << squareRadiusMax;
+        //std::cout << squareRadiusMax;
         //Generate the radius which is the same percentage from the circle but this time from the origin to suqare square
         //https://www.desmos.com/calculator/jvgokzaevl Uses the modulo equation by Raven Jyro Felix[Jehbar Ibarra]
-        double rawSquareRadius{circleRadiusPercent * squareRadiusMax};
+        const double rawSquareRadius{circleRadiusPercent * squareRadiusMax};
 
         return Pair2D<double> {rawSquareRadius * std::cos(angle), rawSquareRadius * std::sin(angle)};
     }

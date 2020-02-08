291f051df9a959aa11855f6f79714e23d61d104a
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-08 15:10:39 -0600

    Add accel to position function to utilities

--- InfiniteChargez/src/main/include/Utilities.h
+++ InfiniteChargez/src/main/include/Utilities.h
@@ -2,9 +2,11 @@
 #define YEET_UTILITIES_H
 #include <utility>
 #include "Pair2D.h"
-
+#include <units/units.h>
+#include <chrono>
 namespace utilities
 {
     Pair2D<double> squarify(double x, double y);
+    units::meter_t simpleAccelToPos(units::meters_per_second_squared_t accel, std::chrono::duration<double> delta);
 }
 #endif

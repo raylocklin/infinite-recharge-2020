d1bff1e4744822f5451428410fc201adfdf84dfe
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-08 18:02:29 -0600

    Update Robot update position

--- InfiniteChargez/src/main/cpp/Odometry.cpp
+++ InfiniteChargez/src/main/cpp/Odometry.cpp
@@ -1,8 +1,29 @@
 #include "Robot.h"
+#include "Pair2D.h"
+#include "Utilities.h"
 #include <chrono>
+#include <units/units.h>
+#include <cmath>
 
 void Robot::updatePos(duration_t delta)
 {
-    //std::cout << delta.count();
-    //std::cout << "reeee" << '\n';
+    //std::cout << leGyroscope.GetGyroAngleZ();
+    units::degree_t leAngle{leGyroscope.GetGyroAngleZ()};
+    units::meter_t xDelta{
+        utilities::simpleAccelToPos(units::standard_gravity_t{-leAccelerometer.GetX()}, delta) * std::cos(units::radian_t{leAngle}.to<double>())  +
+        utilities::simpleAccelToPos(units::standard_gravity_t{-leAccelerometer.GetY()}, delta) * std::sin(units::radian_t{leAngle}.to<double>())};
+
+    units::meter_t yDelta{
+        utilities::simpleAccelToPos(units::standard_gravity_t{-leAccelerometer.GetX()}, delta) * std::sin(units::radian_t{leAngle}.to<double>())  +
+        utilities::simpleAccelToPos(units::standard_gravity_t{-leAccelerometer.GetY()}, delta) * std::cos(units::radian_t{leAngle}.to<double>())};
+
+    frc::Translation2d leTranslate{xDelta, yDelta};
+
+
+    leRobotPosition = frc::Pose2d{leTranslate + leRobotPosition.Translation(), frc::Rotation2d{leAngle}};
+    std::cout << delta.count();
+    std::cout << "LeRobotPosition: X: " << leRobotPosition.Translation().X().to<double>() << '\n'
+        << "Le RobotPosition: Y" << leRobotPosition.Translation().Y().to<double>() << '\n'
+        << "Le RobotPosition: Rot: " << leRobotPosition.Rotation().Radians().to<double>() << '\n' << '\n';
 }
+

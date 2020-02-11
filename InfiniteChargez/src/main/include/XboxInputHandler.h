a23a8608712f0929f9545d58d07b2604c6b0c704
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-11 14:48:20 -0600

    Fix getters for buttons in Inputhandler

--- InfiniteChargez/src/main/include/XboxInputHandler.h
+++ InfiniteChargez/src/main/include/XboxInputHandler.h
@@ -12,6 +12,7 @@ namespace utilities
     using joystick_t = utilities::Pair2D<double>;
     using bitmask_t = std::uint8_t;
     using button_t = std::uint8_t;
+    using trigger_t = double;
 
     private:
     static constexpr bitmask_t ButtonStateMask{1 << 0};
@@ -30,43 +31,44 @@ namespace utilities
         //Button Functions
         //Button A
         void setButtonAState(bool state) {(state) ? m_buttonA |= ButtonStateMask : m_buttonA &= ~ButtonStateMask;}
-        bool getButtonAState(bool state) {return m_buttonA & ButtonStateMask;}
+        bool getButtonAState() {return m_buttonA & ButtonStateMask;}
 
         void setButtonAPressed(bool state) {(state) ? m_buttonA |= ButtonPressedMask : m_buttonA &= ~ButtonPressedMask;}
-        bool getButtonAPressed(bool state) {return m_buttonA & ButtonPressedMask;}
+        bool getButtonAPressed() {return m_buttonA & ButtonPressedMask;}
 
         void setButtonAReleased(bool state) {(state) ? m_buttonA |= ButtonReleasedMask : m_buttonA &= ~ButtonReleasedMask;}
-        bool getButtonAReleased(bool state) {return m_buttonA & ButtonReleasedMask;}
+        bool getButtonAReleased() {return m_buttonA & ButtonReleasedMask;}
 
         //ButtonB
         void setButtonBState(bool state) {(state) ? m_buttonB |= ButtonStateMask : m_buttonB &= ~ButtonStateMask;}
-        bool getButtonBState(bool state) {return m_buttonB & ButtonStateMask;}
+        bool getButtonBState() {return m_buttonB & ButtonStateMask;}
 
         void setButtonBPressed(bool state) {(state) ? m_buttonB |= ButtonPressedMask : m_buttonB &= ~ButtonPressedMask;}
-        bool getButtonBPressed(bool state) {return m_buttonB & ButtonPressedMask;}
+        bool getButtonBPressed() {return m_buttonB & ButtonPressedMask;}
 
         void setButtonBReleased(bool state) {(state) ? m_buttonB |= ButtonReleasedMask : m_buttonB &= ~ButtonReleasedMask;}
-        bool getButtonBReleased(bool state) {return m_buttonB & ButtonReleasedMask;}
+        bool getButtonBReleased() {return m_buttonB & ButtonReleasedMask;}
 
 	//ButtonX
         void setButtonXState(bool state) {(state) ? m_buttonX |= ButtonStateMask : m_buttonX &= ~ButtonStateMask;}
-        bool getButtonXState(bool state) {return m_buttonX & ButtonStateMask;}
+        bool getButtonXState() {return m_buttonX & ButtonStateMask;}
 
         void setButtonXPressed(bool state) {(state) ? m_buttonX |= ButtonPressedMask : m_buttonX &= ~ButtonPressedMask;}
-        bool getButtonXPressed(bool state) {return m_buttonX & ButtonPressedMask;}
+        bool getButtonXPressed() {return m_buttonX & ButtonPressedMask;}
 
         void setButtonXReleased(bool state) {(state) ? m_buttonX |= ButtonReleasedMask : m_buttonX &= ~ButtonReleasedMask;}
-        bool getButtonXReleased(bool state) {return m_buttonX & ButtonReleasedMask;}
+        bool getButtonXReleased() {return m_buttonX & ButtonReleasedMask;}
 
 	//ButtonY
         void setButtonYState(bool state) {(state) ? m_buttonY |= ButtonStateMask : m_buttonY &= ~ButtonStateMask;}
-        bool getButtonYState(bool state) {return m_buttonY & ButtonStateMask;}
+        bool getButtonYState() {return m_buttonY & ButtonStateMask;}
 
         void setButtonYPressed(bool state) {(state) ? m_buttonY |= ButtonPressedMask : m_buttonY &= ~ButtonPressedMask;}
-        bool getButtonYPressed(bool state) {return m_buttonY & ButtonPressedMask;}
+        bool getButtonYPressed() {return m_buttonY & ButtonPressedMask;}
 
         void setButtonYReleased(bool state) {(state) ? m_buttonY |= ButtonReleasedMask : m_buttonY &= ~ButtonReleasedMask;}
-        bool getButtonYReleased(bool state) {return m_buttonY & ButtonReleasedMask;}
+        bool getButtonYReleased() {return m_buttonY & ButtonReleasedMask;}
+    
 
 
 
@@ -82,6 +84,11 @@ namespace utilities
         button_t m_buttonB{0};
         button_t m_buttonX{0};
         button_t m_buttonY{0};
+
+        trigger_t m_triggerLeft{0};
+        trigger_t m_triggerRight{0};
+
+
 
     };
 }
